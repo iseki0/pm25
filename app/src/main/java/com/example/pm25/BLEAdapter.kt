6cdf0d2ea5623ccbdd544bc40f51a57fae7b8e78
iseki <admin@iseki.space>
2020-09-18 21:13:29 +0800

    clean up

---
+++ app/src/main/java/com/example/pm25/BLEAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.pm25
+
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class BLEAdapter(context: Context) : BaseAdapter() {
+
+    private val inflater = LayoutInflater.from(context)!!
+    private val list = ArrayList<Pair<String, BluetoothDevice>>()
+    private val addressSet = mutableSetOf<String>()
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val name = list[position].first
+        val view = (convertView ?: inflater.inflate(R.layout.scan_item, parent, false)) as TextView
+        view.text = name
+        return view
+    }
+
+    override fun getItem(position: Int) = list[position].second
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getCount(): Int = list.size
+
+    fun clear() {
+        list.clear()
+    }
+
+    fun putDevice(device: BluetoothDevice) {
+        val address = device.address
+        if (address in addressSet) return
+        val name = (device.name ?: "<no name>") + " ($address)"
+        addressSet.add(address)
+        list.add(name to device)
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/example/pm25/BleScanner.kt
@@ -0,0 +1,29 @@
+package com.example.pm25
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.util.Log
+
+class BleScanner(val context: Context, val bluetoothAdapter: BluetoothAdapter) {
+    val viewAdapter = BLEAdapter(context)
+    var scanning = false
+        private set
+    private val handler = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            viewAdapter.putDevice(result.device)
+        }
+    }
+
+    fun doScan() {
+        Log.d("scanner", "start")
+        runCatching { bluetoothAdapter.bluetoothLeScanner.startScan(handler) }
+    }
+
+    fun stopScan() {
+        Log.d("scanner", "stop")
+        runCatching { bluetoothAdapter.bluetoothLeScanner.stopScan(handler) }
+    }
+
+}
